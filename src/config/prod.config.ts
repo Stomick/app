11f1096dbab36ec05f8513fc2e1108f10eb8df9b
Stomick <imac@iMacs-iMac.local>
2018-01-11 12:46:06 +0200

    Время разбито на отрезки !!!

--- src/config/prod.config.ts
+++ src/config/prod.config.ts
@@ -1,7 +1,7 @@
 export default {
   // 'API_PATH' : 'http://weev.ru/api/',
   // 'API_PATH' : 'http://localhost:8080/api/',
-  'API_PATH' : 'https://api.weev.ru/',
-  'API_ERROR' : 'https://social-event-api.herokuapp.com/error',
-  'paymentURL' : "https://admin.weev.ru/api/payment/payment"
+  'API_PATH' : '//api.weev.ru/',
+  'API_ERROR' : '//social-event-api.herokuapp.com/error',
+  'paymentURL' : "//admin.weev.ru/api/payment/payment"
 }
